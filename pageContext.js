751781c6bd527f145d29684df10cf846e263b94a
Mihir Kumar <mkumar12@illinois.edu>
2018-08-01 13:24:02 -0500

    Added buttons, tables and code to modify elems based on evaluation

--- pageContext.js
+++ pageContext.js
@@ -3,23 +3,32 @@
 function evaluateRules() {
   console.log('Entered evaluateRules()');
   var doc = window.document;
-  var ruleset = OpenAjax.a11y.RulesetManager.getRuleset();
+  var ruleset = OpenAjax.a11y.RulesetManager.getRuleset("ARIA_STRICT");
   var evaluator_factory = OpenAjax.a11y.EvaluatorFactory.newInstance();
   evaluator_factory.setParameter('ruleset', ruleset);
   evaluator_factory.setFeature('eventProcessing', 'fae-util');
   evaluator_factory.setFeature('groups', 7);
   var evaluator = evaluator_factory.newEvaluator();
   var evaluation = evaluator.evaluate(doc, doc.title, doc.location.href);
-  var out = evaluation.toJSON(true);
-  console.log(out);
-  return out;
+  // var out = evaluation.toJSON(true);
+  console.log(evaluation.url);
+  return evaluation;
+}
+
+function getRequiredData(evaluation) {
+  var outputObject = new Object();
+  outputObject.url = evaluation.getURL();
+  outputObject.ruleset = evaluation.getRuleset().getId();
+  console.log(evaluation.getRuleset().getId());
+  return outputObject;
 }
 
 browser.runtime.onMessage.addListener(request => {
   console.log("Message from the background script:");
   console.log(request.clicked);
 
-  var resultsJSON = evaluateRules();
+  var evaluation = evaluateRules();
+  var output = getRequiredData(evaluation);
   
-  return Promise.resolve({response: resultsJSON});
+  return Promise.resolve({response: output});
 });
--- sidebar/panel.js
+++ sidebar/panel.js
@@ -6,14 +6,22 @@ function onError(error) {
   console.error(`Error: ${error}`);
 }
 
+function changePanelElements(receivedObject) {
+  document.getElementById("location").innerHTML = "Location: " + receivedObject.url;
+  document.getElementById("ruleset").innerHTML = "Ruleset: " + receivedObject.ruleset;
+}
+
 function sendMessageToTabs(tabs) {
   for (let tab of tabs) {
     browser.tabs.sendMessage(
       tab.id,
       {clicked: true}
     ).then(response => {
-      console.log("Message from the content script:");
-      alert(response.response);
+      var receivedObject = response.response;
+      changePanelElements(receivedObject);
+      // document.getElementById("location").innerHTML = "Location: " + receivedObject.url;
+      // alert(locationElem.innerHTML);
+      // alert(receivedObject.url);
     }).catch(onError);
   }
 }
