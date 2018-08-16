bd7ef17d4a394033e3c96c65c480366618851196
Jon Gunderson <jongund@illinois.edu>
2018-08-16 14:38:35 -0500

    added summary information

--- pageContext.js
+++ pageContext.js
@@ -5,52 +5,106 @@ function handleError(error) {
 }
 
 window.onload = function notifyPanel() {
+  console.log('[onload][Start]');
+
   // to be run when the window finishes loading
-  var outputForPanel = processing();
+  var aiResponse = summary();
+
   var sending = browser.runtime.sendMessage({
-    messageForPanel: outputForPanel
+    messageForPanel: aiResponse
   });
   sending.then(handleError);
 }
 
-function evaluateRules() {
+function evaluateRules(ruleset) {
+  console.log('[evaluateRules][Start]: ' + ruleset);
+
+  if (ruleset !== 'ARIA_TRANS' && ruleset !== 'ARIA_STRICT') {
+    ruleset = 'ARIA_STRICT';
+  }
+
   // evaluation script
-  console.log('Entered evaluateRules()');
   var doc = window.document;
-  var ruleset = OpenAjax.a11y.RulesetManager.getRuleset("ARIA_STRICT");
+  var rs = OpenAjax.a11y.RulesetManager.getRuleset(ruleset);
   var evaluator_factory = OpenAjax.a11y.EvaluatorFactory.newInstance();
-  evaluator_factory.setParameter('ruleset', ruleset);
+  evaluator_factory.setParameter('ruleset', rs);
   evaluator_factory.setFeature('eventProcessing', 'fae-util');
   evaluator_factory.setFeature('groups', 7);
   var evaluator = evaluator_factory.newEvaluator();
-  var evaluation = evaluator.evaluate(doc, doc.title, doc.location.href);
+  var evaluationResult = evaluator.evaluate(doc, doc.title, doc.location.href);
   // var out = evaluation.toJSON(true);
-  console.log(evaluation.url);
-  return evaluation;
+  console.log('[evaluateRules][End]: ' + evaluationResult);
+  return evaluationResult;
 }
 
-function getRequiredData(evaluation) {
+function getCommonData(evaluationResult) {
+  console.log('[getCommonData][Start]');
   // gets required data from evaluation object to return to panel
-  var outputObject = new Object();
-  outputObject.url = evaluation.getURL();
-  outputObject.ruleset = evaluation.getRuleset().getId();
-  console.log(evaluation.getRuleset().getId());
-  return outputObject;
+  var aiResponse = new Object();
+  aiResponse.url = evaluationResult.getURL();
+  aiResponse.ruleset = evaluationResult.getRuleset().getId();
+  console.log('[getCommonData][End]: ' + aiResponse);
+  return aiResponse;
 }
 
-function processing() {
-  var evaluation = evaluateRules();
-  var output = getRequiredData(evaluation);
+function addSummaryData(aiResponse, evaluationResult) {
+  console.log('[getSummaryData][Start]');
+  var ruleGroupResult = evaluationResult.getRuleResultsAll();
+  var ruleSummaryResult = ruleGroupResult.getRuleResultsSummary();
 
-  return output;
+  aiResponse.violations    = ruleSummaryResult.violations;
+  aiResponse.warnings      = ruleSummaryResult.warnings;
+  aiResponse.manual_checks = ruleSummaryResult.manual_checks;
+  aiResponse.passed        = ruleSummaryResult.passed;
+
+  console.log('[getSummaryData][End]');
+}
+
+function addRuleCategoryData(aiResponse, evaluationResult) {
+  console.log('[addRuleCategoryData][Start]');
+  var landmarkResults = evaluationResult.getRuleResultsByCategory(OpenAjax.a11y.RULE_CATEGORIES.LANDMARKS);
+
+  console.log('[addRuleCategoryData][End]');
+}
+
+function summary(ruleset) {
+  console.log('[summary][Start]');
+  var evaluationResult = evaluateRules(ruleset);
+  var aiResponse = getCommonData(evaluationResult);
+  addSummaryData(aiResponse, evaluationResult);
+  aiResponse.option = 'summary'
+
+  console.log('[summary][URL]: '     + aiResponse.url);
+  console.log('[summary][Ruleset]: ' + aiResponse.ruleset);
+  console.log('[summary][End]: '     + aiResponse);
+  return aiResponse;
 }
 
 browser.runtime.onMessage.addListener(request => {
   // to be executed on receiving messages from the panel
-  console.log("Message from the background script:");
-  console.log(request.clicked);
 
-  var outputForPanel = processing();
+  var aiResponse;
 
-  return Promise.resolve({response: outputForPanel});
+  if (typeof request.option !== 'string') {
+    request.option = 'summary';
+  }
+
+  if (typeof request.ruleset !== 'string') {
+    request.ruleset = 'ARIA_STRICT';
+  }
+
+  console.log("[onMessage][start]");
+  console.log('[onMessage][option]: ' + request.option);
+  console.log('[onMessage][ruleset]: ' + request.ruleset);
+
+  switch (request.option) {
+    case 'summary':
+    default:
+      aiResponse = summary(request.ruleset);
+    break;
+
+  }
+
+  console.log('[onMessage][End]:' + aiResponse);
+  return Promise.resolve({response: aiResponse});
 });
--- sidebar/panel.js
+++ sidebar/panel.js
@@ -9,19 +9,30 @@ function onError(error) {
 browser.runtime.onMessage.addListener(notify);
 
 function notify(message) {
-  changePanelElements(message.messageForPanel);
+  if (message.messageForPanel) {
+    changePanelElements(message.messageForPanel);
+  }
 }
 
 function changePanelElements(receivedObject) {
-  document.getElementById("location").innerHTML = "Location: " + receivedObject.url;
-  document.getElementById("ruleset").innerHTML = "Ruleset: " + receivedObject.ruleset;
+  if (receivedObject.option == 'summary') {
+    document.getElementById("location").innerHTML = "Location: " + receivedObject.url;
+    document.getElementById("ruleset").innerHTML = "Ruleset: " + receivedObject.ruleset;
+
+    // update Rule Summary
+    document.getElementById("violations").innerHTML      = receivedObject.violations;
+    document.getElementById("warnings").innerHTML        = receivedObject.warnings;
+    document.getElementById("manual_checks").innerHTML   = receivedObject.manual_checks;
+    document.getElementById("passed").innerHTML          = receivedObject.passed;
+  }
+
 }
 
 function sendMessageToTabs(tabs) {
   for (let tab of tabs) {
     browser.tabs.sendMessage(
       tab.id,
-      {clicked: true}
+      {clicked: true, option: 'summary'}
     ).then(response => {
       var receivedObject = response.response;
       changePanelElements(receivedObject);
@@ -30,6 +41,13 @@ function sendMessageToTabs(tabs) {
 }
 
 evaluateButton.addEventListener("click", function(){
+    browser.tabs.query({
+        currentWindow: true,
+        active: true
+    }).then(sendMessageToTabs).catch(onError);
+});
+
+window.addEventListener("load", function(){
     browser.tabs.query({
         currentWindow: true,
         active: true
