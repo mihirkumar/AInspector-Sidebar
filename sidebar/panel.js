c6096a52c12eb5fbfd5260721a7c2d51ccca005f
Jon Gunderson <jongund@illinois.edu>
2018-08-17 16:46:38 -0500

    updated

--- sidebar/panel.js
+++ sidebar/panel.js
@@ -58,8 +58,17 @@ function updateSummaryPanel(evaluationResult) {
 function updateGroupPanel(evaluationResult) {
 
   function addRuleResultRow(id, label, req, v, w, mc, p, h) {
+    var html = '<tr>'
+    html += '  <th><a href="#" id="' + id + '">' + label + '</a></th>';
+    html += '  <td>' + (req ? 'Y' : '') + '</td>';
+    html += '  <td>' + v     + '</td>';
+    html += '  <td>' + w     + '</td>';
+    html += '  <td>' + mc    + '</td>';
+    html += '  <td>' + p     + '</td>';
+    html += '  <td>' + h     + '</td>';
+    html += '</tr>'
 
-
+    return html;
   }
 
   // update Rule Summary
@@ -71,14 +80,14 @@ function updateGroupPanel(evaluationResult) {
   // Update Group Results
 
   var html = '';
-  var node = document.getElementById("group_results");
-
+  var node = document.getElementById("rule_results");
 
   for (let i = 0; i < evaluationResult.ruleResults.length; i++) {
-    var rr = evaluationResult.ruleResults[i];
-
+    var rr = evaluationResult.ruleResults[i];0
     html += addRuleResultRow(rr.id, rr.label, rr.required, rr.violations, rr.warnings, rr.manual_checks, rr.passed, rr.hidden);
   }
+
+  node.innerHTML = html;
 
 }
 
