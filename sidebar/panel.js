53e8fc2975d898d206aaf4df510767376492412e
Mihir Kumar <mkumar12@illinois.edu>
2018-08-23 13:33:30 -0500

    Context menu and storage (not working)

--- sidebar/panel.js
+++ sidebar/panel.js
@@ -1,6 +1,35 @@
 "use strict";
 
 var evaluateButton = document.getElementById('evaluate');
+var radios = document.forms["Persistence"].elements["car"];
+
+var storedValue = browser.storage.local.get("car");
+alert(storedValue);
+
+// for(var i = 0, max_i = radios.length; i < max; i++) {
+//   radios[i].onclick = function() 
+//     var storedData = {"car": this.value};
+//   }
+// }
+
+for(var j = 0, max_j = radios.length; j < max_j; j++) {
+  radios[j].onclick = function() {
+    browser.storage.local.set({"car": this.value});
+    alert(this.value);
+  }
+}
+
+browser.contextMenus.create({
+  id: "ainspector",
+  title: "Run ARIA evaluation",
+  contexts: ["all"],
+});
+
+browser.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "ainspector") {
+    evaluateButton.click();
+  }
+});
 
 function onError(error) {
   console.error(`Error: ${error}`);
@@ -15,6 +44,7 @@ function notify(message) {
 function changePanelElements(receivedObject) {
   document.getElementById("location").innerHTML = "Location: " + receivedObject.url;
   document.getElementById("ruleset").innerHTML = "Ruleset: " + receivedObject.ruleset;
+  exportToPdf();
 }
 
 function sendMessageToTabs(tabs) {
@@ -35,3 +65,9 @@ evaluateButton.addEventListener("click", function(){
         active: true
     }).then(sendMessageToTabs).catch(onError);
 });
+
+function exportToPdf() {
+  var doc = new jsPDF()
+  doc.text('Hello world!', 10, 10)
+  doc.save('a4.pdf')
+}
